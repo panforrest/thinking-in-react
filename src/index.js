3a0b16b0279b190a93cd9d45e9f2fa1b8aaa98a8
panforrest <guoqianp@gmail.com>
2017-08-05 00:34:35 -0400

    class ProductCategoryRow extends React.Component {

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
+      // <div>
+      //   <SearchBar />
+      //   <ProductTable products=this.state.products />
+      // </div>
+
 import React from 'react';
 import './index.css'
 import ReactDOM from 'react-dom'
+
+class ProductCategoryRow extends React.Component {
+  render(){
+    return(
+      <tr>
+        <th colSpan="2">
+          {this.props.category}  
+        </th>
+      </tr>
+    )
+  }
+}
+
+class ProductRow extends React.Component {
+  render(){
+    var name = this.props.product.stocked ? this.props.product.name : <span style={{color: 'red'}}>{this.props.product.name}</span>
+
+    return(
+      <div>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </div>
+    )
+  }
+}
+
+class ProductTable extends React.Component{
+  render(){
+    var rows = [];
+    var lastCategory = null;
+
+    this.props.products.forEach(function(product){
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);  
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    })
+
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
 
 class SearchBar extends React.Component {
   render(){
@@ -22,6 +79,7 @@ class FilterableProductTable extends React.Component{
     return(
       <div>
         <SearchBar />
+        <ProductTable products={this.props.products} />
       </div>
     )
   }
